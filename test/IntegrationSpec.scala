f9a51180e0f1e0b9a60b9c88049f22b1fc9eacae
Thomas Choo <c.thomas.r@gmail.com>
2014-12-30 17:04:22 -0800

    Add integeration testing

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,24 +1,43 @@
-import org.specs2.mutable._
-import org.specs2.runner._
-import org.junit.runner._
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
 
+import play.api.i18n.Messages
 import play.api.test._
-import play.api.test.Helpers._
 
-/**
- * add your integration spec here.
- * An integration test will fire up a whole play application in a real (or headless) browser
- */
+import scala.collection.JavaConversions.asScalaBuffer
+
 @RunWith(classOf[JUnitRunner])
 class IntegrationSpec extends Specification {
 
   "Application" should {
+    val AltRootPathname = "/home"
 
     "work from within a browser" in new WithBrowser {
+      browser.goTo("http://localhost:" + port)
+      browser.$("title").getText must equalTo(Messages("app.title"))
+    }
 
+    "allow navigation to different pages from index" in new WithBrowser(WebDriverFactory(Helpers.FIREFOX)) {
       browser.goTo("http://localhost:" + port)
 
-      browser.pageSource must contain("Your new application is ready.")
+      browser.$("li.menu-item") foreach { menuItem =>
+        menuItem.click()
+        browser.url must equalTo(getPathname(menuItem.getAttribute("data-pathname")))
+      }
+
+      def getPathname(pathname: String): String = if (pathname == AltRootPathname) "/" else pathname
+    }
+
+    "allow browsing to different pages directly" in new WithBrowser {
+      browser.goTo("http://localhost:" + port)
+
+      val pathnames: List[String] = browser.$("li.menu-item").getAttributes("data-pathname").toList
+      pathnames foreach { pathname =>
+        browser.goTo(pathname)
+        browser.pageSource must contain(s"${pathname.substring(1)}-content")
+      }
     }
   }
 }
+
