7f86b89bffc3c3a8c92a9c47baff5deaff213c6c
Thomas Choo <c.thomas.r@gmail.com>
2015-01-02 08:50:28 -0800

    Add server side caching to index page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,14 +1,17 @@
 package controllers
 
+import play.api.Play.current
+import play.api.cache.Cached
 import play.api.mvc.{Action, Controller}
 
 import models.MenuSupport
 
 object Application extends Controller with MenuSupport {
 
-  def index = Action {
-    implicit request =>
-      Ok(views.html.index())
+  def index = Cached("index") {
+    Action {
+      implicit request =>
+        Ok(views.html.index())
+    }
   }
-
 }
