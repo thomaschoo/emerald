32241bab7c018cc173d45aed3b59e39bb641e67a
Thomas Choo <c.thomas.r@gmail.com>
2015-01-02 20:33:05 -0800

    Add unit test for root route

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -5,6 +5,7 @@ import org.specs2.runner.JUnitRunner
 import play.api.test.Helpers._
 import play.api.test.{FakeRequest, WithApplication}
 
+import helpers.TestUtilities._
 import views._
 
 @RunWith(classOf[JUnitRunner])
@@ -14,6 +15,21 @@ class ApplicationSpec extends Specification {
 
     "send 404 on a bad request" in new WithApplication {
       route(FakeRequest(GET, "/bad")) must beNone
+    }
+  }
+
+  "'/' route" should {
+    "respond with index Action" in new WithApplication {
+      val Some(result) = route(FakeRequest(GET, "/"))
+
+      status(result) must equalTo(OK)
+      contentType(result) must beSome("text/html")
+      charset(result) must beSome("utf-8")
+
+      val content = stripWhiteSpaces(contentAsString(result))
+      content must not have length(0)
+      val footer = stripWhiteSpaces(contentAsString(views.html.footer()))
+      content must contain(footer)
     }
   }
 
