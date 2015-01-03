cfc4d4a07ec3c7ec3ba1edd5a43309af8cf61f84
Thomas Choo <c.thomas.r@gmail.com>
2015-01-02 20:40:42 -0800

    Add unit test for contact messages

--- test/views/ContactSpec.scala
+++ test/views/ContactSpec.scala
@@ -4,8 +4,8 @@ import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 
-import play.api.test.FakeApplication
 import play.api.test.Helpers._
+import play.api.test.{FakeApplication, FakeRequest, WithApplication}
 
 import helpers.TestUtilities._
 import helpers.WithRouter
@@ -38,6 +38,16 @@ class ContactSpec extends Specification {
       val content = stripWhiteSpaces(contentAsString(result))
       val footer = stripWhiteSpaces(contentAsString(views.html.footer()))
       content must contain(footer)
+    }
+  }
+
+  "'/contact/messages.js' route" should {
+    "respond with messages Action" in new WithApplication {
+      val Some(result) = route(FakeRequest(GET, "/contact/messages.js"))
+
+      status(result) must equalTo(OK)
+      contentType(result) must beSome("text/javascript")
+      charset(result) must beSome("utf-8")
     }
   }
 }
