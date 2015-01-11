1e82058bea87153643808811c834c2eec21385ce
Thomas Choo <c.thomas.r@gmail.com>
2015-01-10 19:13:00 -0800

    Modify to use Scala primitive

--- app/models/Combo.scala
+++ app/models/Combo.scala
@@ -1,3 +1,3 @@
 package models
 
-case class Combo(title: String, items: Seq[String], servings: String, price: Integer)
+case class Combo(title: String, items: Seq[String], servings: String, price: Int)
