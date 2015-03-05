7cd7c93fc2c26832a50d13f46d5bf69591ef87c3
Thomas Choo <c.thomas.r@gmail.com>
2015-03-04 22:55:10 -0800

    Modify show feast api to handle unfound documents

--- app/controllers/Feast.scala
+++ app/controllers/Feast.scala
@@ -24,7 +24,10 @@ object Feast extends Controller with MenuSupport {
   def show(id: String) = Action.async { implicit request =>
     FeastDao.find(id) map { combo =>
       render {
-        case Accepts.Json() => Ok(Json.toJson(combo))
+        case Accepts.Json() => combo match {
+          case Some(c) => Ok(Json.toJson(c))
+          case None => NotFound(Json.parse("{}"))
+        }
       }
     }
   }
--- app/services/FeastDao.scala
+++ app/services/FeastDao.scala
@@ -4,13 +4,12 @@ import scala.concurrent.Future
 
 import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.json.Json
 import play.modules.reactivemongo.ReactiveMongoPlugin
 import play.modules.reactivemongo.json.collection.JSONCollection
 import play.modules.reactivemongo.json.BSONFormats._
 
 import reactivemongo.api.QueryOpts
-import reactivemongo.bson.BSONObjectID
+import reactivemongo.bson.{BSONObjectID, BSONDocument}
 
 import models.Combo
 
@@ -19,15 +18,18 @@ object FeastDao {
   private def collection: JSONCollection = ReactiveMongoPlugin.db.collection[JSONCollection]("combos")
 
   def find(id: String): Future[Option[Combo]] = {
-    collection
-      .find(Json.obj("_id" -> BSONObjectID(id)))
-      .cursor[Combo]
-      .headOption
+    val combo =
+      for {
+        objectId <- BSONObjectID.parse(id)
+        c = collection.find(BSONDocument("_id" -> objectId)).one[Combo]
+      } yield c
+
+    combo getOrElse Future { None }
   }
 
   def findAll(offset: Int, limit: Int): Future[List[Combo]] = {
     collection
-      .find(Json.obj("type" -> "feast"))
+      .find(BSONDocument("type" -> "feast"))
       .options(QueryOpts(offset, limit))
       .cursor[Combo]
       .collect[List](limit)
