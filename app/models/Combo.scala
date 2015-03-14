6be9a5369ec4e3303f14e82085a6341505f27da0
Thomas Choo <c.thomas.r@gmail.com>
2015-03-13 22:45:11 -0700

    Separate domain and database dependency
    Any references to BSON should be kept out of the model. This
    separation allows the model to be cleaner and independent of
    the underlying database.

--- app/models/Combo.scala
+++ app/models/Combo.scala
@@ -2,28 +2,15 @@ package models
 
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
-import play.modules.reactivemongo.json.BSONFormats._
-
-import reactivemongo.bson.BSONObjectID
 
 case class Combo(id: String, title: String, items: Seq[String], servings: String, price: Int)
 
 object Combo {
-  val comboReads: Reads[Combo] = (
-    (__ \ "_id").read[BSONObjectID] map(_.stringify) and
-      (__ \ "title").read[String] and
-      (__ \ "items").read[Seq[String]] and
-      (__ \ "servings").read[String] and
-      (__ \ "price").read[Int]
-    )(Combo.apply _)
-
-  val comboWrites: Writes[Combo] = (
+  implicit val comboWrites: Writes[Combo] = (
     (__ \ "id").write[String] and
       (__ \ "title").write[String] and
       (__ \ "items").write[Seq[String]] and
       (__ \ "servings").write[String] and
       (__ \ "price").write[Int]
     )(unlift(Combo.unapply))
-
-  implicit val comboFormat: Format[Combo] = Format(comboReads, comboWrites)
 }
--- app/services/FeastDao.scala
+++ app/services/FeastDao.scala
@@ -4,16 +4,25 @@ import scala.concurrent.Future
 
 import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{Reads, __}
 import play.modules.reactivemongo.ReactiveMongoPlugin
-import play.modules.reactivemongo.json.collection.JSONCollection
 import play.modules.reactivemongo.json.BSONFormats._
+import play.modules.reactivemongo.json.collection.JSONCollection
 
 import reactivemongo.api.QueryOpts
-import reactivemongo.bson.{BSONObjectID, BSONDocument}
+import reactivemongo.bson.{BSONDocument, BSONObjectID}
 
 import models.Combo
 
 object FeastDao {
+  implicit val comboReads: Reads[Combo] = (
+    (__ \ "_id").read[BSONObjectID] map (_.stringify) and
+      (__ \ "title").read[String] and
+      (__ \ "items").read[Seq[String]] and
+      (__ \ "servings").read[String] and
+      (__ \ "price").read[Int]
+    )(Combo.apply _)
 
   private def collection: JSONCollection = ReactiveMongoPlugin.db.collection[JSONCollection]("combos")
 
